607163d301b7062b01030ba8171e0b636d04ff13
Gen Ichino <zenoplex@gmail.com>
2020-07-07 00:56:45 +0900

    refactor: simplify files

--- src/pages/chapter01/03_materials_light.tsx
+++ src/pages/chapter01/03_materials_light.tsx
@@ -1,12 +1,67 @@
 import * as React from 'react';
-import dynamic from 'next/dynamic';
+import * as Three from 'three';
+import { Canvas } from 'react-three-fiber';
 
-const Canvas = dynamic(import('~/components/chapter01/MaterialsLight'), {
-  ssr: false,
-});
+const Plain = (): JSX.Element => {
+  return (
+    <mesh
+      visible
+      position={[15, 0, 0]}
+      rotation={[-0.5 * Math.PI, 0, 0]}
+      receiveShadow>
+      <planeGeometry attach="geometry" args={[60, 20]} />
+      <meshLambertMaterial attach="material" color={0xaaaaaa} />
+    </mesh>
+  );
+};
+
+const Cube = (): JSX.Element => {
+  return (
+    <mesh visible position={[-4, 2, 0]} castShadow>
+      <boxGeometry attach="geometry" args={[4, 4, 4]} />
+      <meshLambertMaterial attach="material" color={0xff0000} />
+    </mesh>
+  );
+};
+
+const Sphere = (): JSX.Element => {
+  return (
+    <mesh visible position={[20, 4, 2]} castShadow>
+      <sphereGeometry attach="geometry" args={[4, 20, 20]} />
+      <meshLambertMaterial attach="material" color={0x7777ff} />
+    </mesh>
+  );
+};
 
 const Page = (): JSX.Element => {
-  return <Canvas />;
+  return (
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 1000,
+        position: [-30, 40, 30],
+      }}
+      onCreated={({ gl }) => {
+        gl.setClearColor(new Three.Color(0x000000));
+      }}>
+      <Plain />
+      <Cube />
+      <Sphere />
+
+      <spotLight
+        color={0xffffff}
+        position={[-40, 40, -15]}
+        castShadow
+        shadowMapWidth={1024}
+        shadowMapHeight={1024}
+        shadowCameraFar={130}
+        shadowCameraNear={40}
+      />
+
+      <ambientLight color={0x353535} />
+    </Canvas>
+  );
 };
 
 export default Page;
