a25176ddeccce6b1359004aa38b5b9c69552e3fd
Gen Ichino <zenoplex@gmail.com>
2020-09-03 21:44:43 +0900

    feat: update sample

--- src/pages/chapter03/05_hemisphere_light.tsx
+++ src/pages/chapter03/05_hemisphere_light.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
-import { Canvas, useFrame, useResource, useLoader } from 'react-three-fiber';
+import { Canvas, useFrame, useLoader } from 'react-three-fiber';
 import * as Three from 'three';
-import DatGui, {
-  DatNumber,
-  DatColor,
-  DatSelect,
-  DatBoolean,
-} from 'react-dat-gui';
+import DatGui, { DatNumber, DatColor, DatBoolean } from 'react-dat-gui';
 import { TrackballControls, Stats } from 'drei';
 
 const Plane = React.forwardRef<Three.Mesh>(
   (_, ref): JSX.Element => {
     const texture = useLoader(Three.TextureLoader, '/grasslight-big.jpg');
+    /* eslint-disable functional/immutable-data */
     texture.wrapS = Three.RepeatWrapping;
     texture.wrapT = Three.RepeatWrapping;
+    /* eslint-enable functional/immutable-data */
+    texture.repeat.set(10, 10);
 
     return (
       <mesh
@@ -90,46 +88,18 @@ const Sphere = React.forwardRef<Three.Mesh, SphereProps>(
 // eslint-disable-next-line functional/immutable-data
 Sphere.displayName = 'Sphere';
 
-type DirectionalLightProps = {
-  readonly bouncingSpeed: number;
-  readonly target: Three.Mesh | null;
-  readonly canCastShadow: boolean;
-  readonly color: string;
-};
-
 const Page = (): JSX.Element => {
-  const [planeRef, plane] = useResource<Three.Mesh>();
-  const [cubeRef, cube] = useResource<Three.Mesh>();
-  const [sphereRef, sphere] = useResource<Three.Mesh>();
+  const planeRef = React.useRef<Three.Mesh>(null);
+  const cubeRef = React.useRef<Three.Mesh>(null);
+  const sphereRef = React.useRef<Three.Mesh>(null);
   const [state, setState] = React.useState({
-    ambientColor: '#0c0c0c',
-    directionalColor: '#ff5808',
-    distance: 100,
-    intensity: 0.5,
+    color: '#0000ff',
+    groundColor: '#00ff00',
+    intensity: 0.6,
+    isHemisphereVisible: true,
     rotationSpeed: 0.03,
     bouncingSpeed: 0.03,
-    canCastShadow: true,
-    target: 'Plane',
   });
-  const [target, setTarget] = React.useState<Three.Mesh | null>(null);
-
-  React.useEffect(() => {
-    /* eslint-disable functional/immutable-data */
-    switch (state.target) {
-      case 'Plane':
-        setTarget(plane);
-        break;
-      case 'Sphere':
-        setTarget(sphere);
-        break;
-      case 'Cube':
-        setTarget(cube);
-        break;
-      default:
-        setTarget(plane);
-    }
-    /* eslint-enable functional/immutable-data */
-  }, [cube, plane, sphere, state.target]);
 
   return (
     <>
@@ -137,7 +107,7 @@ const Page = (): JSX.Element => {
         shadowMap
         camera={{
           fov: 45,
-          position: [-80, 80, 80],
+          position: [-30, 40, 30],
           near: 0.1,
           far: 1000,
         }}
@@ -149,13 +119,17 @@ const Page = (): JSX.Element => {
           <Cube ref={cubeRef} rotationSpeed={state.rotationSpeed} />
           <Sphere ref={sphereRef} bouncingSpeed={state.bouncingSpeed} />
 
-          <spotLight args={[0xcccccc]} position={[-40, 60, -10]} />
+          <spotLight color={0xcccccc} position={[-40, 60, -10]} />
           <hemisphereLight
+            visible={state.isHemisphereVisible}
             args={[0x0000ff, 0x00ff00, 0.6]}
             position={[0, 500, 0]}
+            color={state.color}
+            intensity={state.intensity}
+            groundColor={new Three.Color(state.groundColor)}
           />
           <directionalLight
-            args={['#ffffff']}
+            color={0xffffff}
             castShadow
             shadow-camera-near={0.1}
             shadow-camera-far={200}
@@ -166,19 +140,17 @@ const Page = (): JSX.Element => {
             shadow-mapSize-width={2048}
             shadow-mapSize-height={2048}
           />
+          <TrackballControls />
+          <Stats />
         </React.Suspense>
-        <TrackballControls />
-        <Stats />
       </Canvas>
       <DatGui data={state} onUpdate={setState}>
-        <DatColor path="ambientColor" />
-        <DatColor path="directionalColor" />
+        <DatColor path="color" />
+        <DatColor path="groundColor" />
         <DatNumber path="intensity" min={0} max={3} step={0.1} />
-        <DatNumber path="distance" min={0} max={100} step={1} />
         <DatNumber path="rotationSpeed" min={0.01} max={0.1} step={0.01} />
         <DatNumber path="bouncingSpeed" min={0.01} max={0.1} step={0.01} />
-        <DatBoolean path="canCastShadow" />
-        <DatSelect path="target" options={['Plane', 'Sphere', 'Cube']} />
+        <DatBoolean path="isHemisphereVisible" />
       </DatGui>
     </>
   );
